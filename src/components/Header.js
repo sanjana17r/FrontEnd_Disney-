2d741a455b9d1ba61e11e6c5856c19734a1dfb6c
Sanjana <sanjana99.r@gmail.com>
2021-05-09 20:35:32 +0530

    newPush

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,55 @@
 import React from 'react'
+import { findRenderedComponentWithType } from 'react-dom/test-utils'
+import {auth, provider} from '../firebase';
 import styled from 'styled-components'
+import {useHistory} from 'react-router-dom'
+import {
+    selectUserEmail,
+    selectUserName,
+    selectUserPhoto,
+    setUserLogin,
+    setSignOut
+} from '../features/user/userSlice'
+import {useDispatch, useSelector} from "react-redux"
+
 
 function Header() {
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    const history = useHistory();
+    const userPhoto = useSelector(selectUserPhoto);
+    
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+        .then((result) => {
+            //console.log(result);
+            let user = result.user
+            dispatch(setUserLogin({
+                name: user.displayName,
+                email: user.email,
+                photo: user.photoURL
+            }));
+        })
+    }
+    const signOut = () => {
+        auth.signOut()
+        .then(() => {
+            dispatch(setSignOut());
+            history.push("/login")
+        })
+    }
+
     return (
         <Nav>
             <Logo src="/images/logo.svg" />
+            {
+                !userName ?(
+                <LoginContainer>
+                <Login onClick = {signIn}>Login</Login>
+                </LoginContainer>):
+                <>
+
+                
             <NavMenu>
                 <a>
                     <img src="/images/home-icon.svg" />
@@ -31,8 +76,11 @@ function Header() {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
-            <UserImg src="https://lh3.googleusercontent.com/a-/AOh14GhPEWpoUbB_IZyXZnBokOFaXzn0RKzgXo7jfxx8GQ=s96-c"/>
-            
+            <UserImg 
+            onClick = {signOut}
+            src="https://lh3.googleusercontent.com/a-/AOh14GhPEWpoUbB_IZyXZnBokOFaXzn0RKzgXo7jfxx8GQ=s96-c"/>
+            </>
+            }
         </Nav>
     )
 }
@@ -97,4 +145,25 @@ const UserImg = styled.img`
     height: 48px;
     border-radius: 50%;
     cursor: pointer;
+`
+const Login = styled.div `
+border: 1px solid #f9f9f9;
+    padding: 8px 16px;
+    border-radius: 4px;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+    background-color: rgba(0,0,0,0.6);
+    transition: all 0.2s ease 0s;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
+`
+const LoginContainer = styled.div `
+flex: 1;
+display: flex;
+justify-content: flex-end;
 `
